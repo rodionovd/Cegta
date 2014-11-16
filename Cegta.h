060882ed79cd6f8bb2bc8cd018a0863e186e4dbb
rodionovd <i.am.rodionovd@gmail.com>
2014-11-17 01:39:40 +0600

    Add require() helper
    * Fix #3

--- Cegta.h
+++ Cegta.h
@@ -55,9 +55,10 @@
 			argv[CEGTA_ARGV_TEST_RESULTS_INDEX] = (char *)EXIT_SUCCESS; \
 		} \
 		const char *__context = NULL; \
-		__block const char *__it = NULL; \
-		__block int __spec_tests = 0, __spec_test_passed = 0;\
+		__unused __block const char *__it = NULL; \
+		__unused __block int __spec_tests = 0, __spec_test_passed = 0;\
 		fprintf(stdout, "Begin spec <%s>\n", #specname);
+
 /**
  * Completes a single spec.
  *
@@ -91,74 +92,111 @@
 			__spec_test_passed += __it_test_passed; \
 		}
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-value"
+
 // The names of these macros is the only thing that matter here
 #define toBe(value) (value)
+#define _toBe(value) (value)
 #define notToBe(value) (value)
+#define _notToBe(value) (value)
 #define toBeLike(value) (value)
+#define _toBeLike(value) (value)
 #define notToBeLike(value) (value)
+#define _notToBeLike(value) (value)
 
-// Print an error if the given expectation was not fulfilled
-#define CegtaExpectation(that, this, this_as_str, format_str, fulfillTo, fulfillNotTo) \
-		++__it_test_count; \
-		if ((strstr(this_as_str, "notTo") == this_as_str) && !fulfillNotTo) { \
-			fprintf(stdout, "\t* [%s, L%d]\n\t|\texpected %s %s\n", \
-				__FILE__, __LINE__, #that, this_as_str); \
-		} else \
-		if ((strstr(this_as_str, "to") == this_as_str) && !fulfillTo) { \
-			fprintf(stdout, "\t* [%s, L%d]\n\t|\texpected %s %s -> "format_str \
-				"\n\t|\t     got %s is("format_str")\n", \
-					__FILE__, __LINE__, #that, this_as_str, this, #that, that); \
-		} else { \
-			++__it_test_passed; \
-		}
+// General testing: print an error if an expectation can't be fulfilled
+#define CegtaStatement(that, this, this_as_str, format_str, fulfillTo, fulfillNotTo) \
+		({ \
+			++__it_test_count; \
+			int __it_before_tests_passed = __it_test_passed; \
+			if (((strstr(this_as_str, "notTo") == this_as_str) || strstr(this_as_str, "_notTo") == this_as_str) \
+				&& !fulfillNotTo) { \
+				fprintf(stdout, "\t* [%s, L%d]\n\t|\texpected %s %s\n", \
+					__FILE__, __LINE__, #that, (this_as_str[0] == '_') ? this_as_str+1 : this_as_str); \
+			} else \
+			if (((strstr(this_as_str, "to") == this_as_str) || (strstr(this_as_str, "_to") == this_as_str)) \
+				&& !fulfillTo) { \
+				fprintf(stdout, "\t* [%s, L%d]\n\t|\texpected %s %s -> "format_str \
+					"\n\t|\t     got %s is("format_str")\n", \
+						__FILE__, __LINE__, #that, (this_as_str[0] == '_') ? this_as_str+1 : this_as_str, \
+						this, #that, that); \
+			} else { \
+				++__it_test_passed; \
+			} \
+			(__it_before_tests_passed != __it_test_passed); \
+		})
 
+// Userland helpers for int
 #define expectInt(that, this) \
-		CegtaExpectation(that, this, #this, "%d",  ((that) == (this)), ((that) != (this)))
+		({ \
+			CegtaStatement(that, this, #this, "%d", ((that) == (this)), ((that) != (this))); \
+		})
+#define requireInt(that, this) \
+		do { \
+			if (!expectInt(that, _##this)) return; \
+		} while (0)
 
+// Userland helpers for double
 #define expectDouble(that, this) \
-		CegtaExpectation(that, this, #this, "%lf", ({ \
-			int result; \
-			if (strstr(#this, "Like") != NULL) { \
-				result = (fabs((that)-(this)) <= CEGTA_EPSILON); \
-			} else { \
-				result = ((that) == (this)); \
-			} \
-			result; \
-		}), ({ \
-			int result; \
-			if (strstr(#this, "Like") != NULL) { \
-				result = (fabs((that)-(this)) > CEGTA_EPSILON); \
-			} else { \
-				result = ((that) != (this)); \
-			} \
-			result; \
-		}))
+		({ \
+			CegtaStatement(that, this, #this, "%lf", ({ \
+				int result; \
+				if (strstr(#this, "Like") != NULL) { \
+					result = (fabs((that)-(this)) <= CEGTA_EPSILON); \
+				} else { \
+					result = ((that) == (this)); \
+				} \
+				result; \
+			}), ({ \
+				int result; \
+				if (strstr(#this, "Like") != NULL) { \
+					result = (fabs((that)-(this)) > CEGTA_EPSILON); \
+				} else { \
+					result = ((that) != (this)); \
+				} \
+				result; \
+			})); \
+		})
+#define requireDouble(that, this) \
+		do { \
+			if (!expectDouble(that, _##this)) return; \
+		} while (0)
 
+// Userland helpers for char*
 #define expectString(that, this) \
-		CegtaExpectation(that, this, #this, "%s", ({ \
-			int result; \
-			if (this == NULL || that == NULL) { \
-				result = (that == this); \
-			} else { \
-				if (strstr(#this, "Like") != NULL) { \
-					result = (strcasecmp(that, this) == 0); \
+		({ \
+			CegtaStatement(that, this, #this, "%s", ({ \
+				int result; \
+				if (this == NULL || that == NULL) { \
+					result = (that == this); \
 				} else { \
-					result = (strcmp(that, this) == 0); \
+					if (strstr(#this, "Like") != NULL) { \
+						result = (strcasecmp(that, this) == 0); \
+					} else { \
+						result = (strcmp(that, this) == 0); \
+					} \
 				} \
-			} \
-			result; \
-		}), ({ \
-			int result; \
-			if (this == NULL || that == NULL) { \
-				result = (that != this); \
-			} else {\
-				if (strstr(#this, "Like") != NULL) { \
-					result = (strcasecmp(that, this) != 0); \
-				} else { \
-					result = (strcmp(that, this) != 0); \
+				result; \
+			}), ({ \
+				int result; \
+				if (this == NULL || that == NULL) { \
+					result = (that != this); \
+				} else {\
+					if (strstr(#this, "Like") != NULL) { \
+						result = (strcasecmp(that, this) != 0); \
+					} else { \
+						result = (strcmp(that, this) != 0); \
+					} \
 				} \
-			} \
-			result; \
-		}))
+				result; \
+			})); \
+		})
+#define requireString(that, this) \
+		do { \
+			if (!expectString(that, _##this)) return; \
+		} while (0)
+
+#pragma clang diagnostic pop
 
 #endif // #ifndef CEGTA_H
